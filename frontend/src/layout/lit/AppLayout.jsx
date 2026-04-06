309f17e3b6cf0ff166827c247096e1b1902f8dd5
Spark <vraymond@sparkfusiondigital.com>
2026-04-06 17:43:25 -0400

    Update AppLayout.jsx

--- frontend/src/layout/lit/AppLayout.jsx
+++ frontend/src/layout/lit/AppLayout.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import AppSidebar from "@/layout/lit/AppSidebar.jsx";
 import AppHeader from "@/layout/lit/AppHeader.jsx";
 
 export default function AppLayout({ children }) {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
   return (
-    <div className="min-h-screen">
-      <AppHeader sidebarOpen={true} setSidebarOpen={() => {}} />
-      <main className="p-6">{children}</main>
+    <div className="min-h-screen flex">
+      <AppSidebar
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+      />
+
+      <div className="min-h-screen flex-1 flex flex-col">
+        <AppHeader
+          sidebarOpen={sidebarOpen}
+          setSidebarOpen={setSidebarOpen}
+        />
+
+        <main className="p-6 flex-1">
+          {children}
+        </main>
+      </div>
     </div>
   );
 }
