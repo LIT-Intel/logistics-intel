e08609d67c0a3397b249c6cc571e0956cb450134
Spark <vraymond@sparkfusiondigital.com>
2026-04-06 17:21:49 -0400

    Update AppLayout.jsx

--- frontend/src/layout/lit/AppLayout.jsx
+++ frontend/src/layout/lit/AppLayout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import AppHeader from "@/layout/lit/AppHeader.jsx";
 
 export default function AppLayout({ children }) {
   return (
-    <div className="min-h-screen p-6">
-      {children}
+    <div className="min-h-screen">
+      <AppHeader sidebarOpen={true} setSidebarOpen={() => {}} />
+      <main className="p-6">{children}</main>
     </div>
   );
 }
