471d3e7e39bf5028ef72ed51652f12fe2e58318b
Spark <vraymond@sparkfusiondigital.com>
2026-04-06 17:42:55 -0400

    Update AppSidebar.jsx

--- frontend/src/layout/lit/AppSidebar.jsx
+++ frontend/src/layout/lit/AppSidebar.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const AppSidebar = () => {
+const AppSidebar = ({ sidebarOpen, setSidebarOpen }) => {
   return (
-    <aside className="hidden md:flex md:w-[270px] md:flex-col border-r border-gray-200 bg-white">
+    <aside className="hidden md:flex md:w-[270px] md:flex-col border-r bg-white">
       <div className="p-5 font-semibold">LIT Sidebar Test</div>
     </aside>
   );
